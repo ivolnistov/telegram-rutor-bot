3666523061098154ff5fa5d8557d8f54e670b726
Ilya Volnistov <i.volnistov@gaijin.team>
2021-04-01 21:25:03 +0300

    fix init db connection

--- db/db.py
+++ db/db.py
@@ -1,18 +1,21 @@
-import settings
 import logging
 from os.path import dirname, join as opj
+from typing import TYPE_CHECKING
 
 import settings
 from .connections import cursor
 
+
+if TYPE_CHECKING:
+    from sqlite3 import Connection
 
 log = logging.getLogger(f'{settings.LOG_PREFIX}.sql')
 
 __all__ = ('init',)
 
 
-def init():
-    with cursor() as cur:
+def init(connection: 'Connection'):
+    with cursor(connection) as cur:
         log.info('creating database')
         with open(opj(dirname(__file__), 'schemas/schema.sql'), 'r') as f:
             cur.executescript(f.read())
