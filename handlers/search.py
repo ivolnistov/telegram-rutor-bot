58dd01184c6939a9eacd358c5bf510093eaa937d
Ilya Volnistov <i.volnistov@gaijin.team>
2021-04-10 20:05:30 +0300

    fix db import

--- handlers/search.py
+++ handlers/search.py
@@ -1,6 +1,5 @@
 import settings
-from db import add_search_to_db, delete_search, get_searches, get_search
-from db.users import get_or_create_user_by_chat_id
+import db
 from schedulers import notify_about_new
 from utils import security
 
@@ -16,7 +15,7 @@ __all__ = (
 @security(settings.USERS_WHITE_LIST)
 def search_list(update, context):
     message = ''
-    for search in get_searches(show_empty=True):
+    for search in db.get_searches(show_empty=True):
         message += f'/ds_{search.id} /es_{search.id} /subscribe_{search.id} {search.url} {" ".join(search.cron)}\n'
     if not message:
         message = 'No searches is defined'
@@ -26,7 +25,7 @@ def search_list(update, context):
 @security(settings.USERS_WHITE_LIST)
 def search_execute(update, context):
     id = update.message.text.replace('/es_', '')
-    search = get_search(id)
+    search = db.get_search(id)
     notify_about_new(search)
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'search with id {id} executed')
 
@@ -34,7 +33,7 @@ def search_execute(update, context):
 @security(settings.USERS_WHITE_LIST)
 def search_delete(update, context):
     id = update.message.text.replace('/ds_', '')
-    delete_search(id)
+    db.delete_search(id)
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'search was deleted')
 
 
@@ -48,8 +47,8 @@ def search_add(update, context):
     if len(cron.split(' ')) < 5:
         return context.bot.send_message(chat_id=update.effective_chat.id, text=f'Invalid cron must be like: * * * * *')
     try:
-        user = get_or_create_user_by_chat_id(update.effective_chat.id)
-        id = add_search_to_db(search, cron, user.id)
+        user = db.get_or_create_user_by_chat_id(update.effective_chat.id)
+        id = db.add_search_to_db(search, cron, user.id)
     except ValueError as e:
         return context.bot.send_message(chat_id=update.effective_chat.id, text=str(e))
     context.bot.send_message(chat_id=update.effective_chat.id, text=f'/subscribe_{id} search with id {id} added')
