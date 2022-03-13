b981eb3146c83ea5aa97bfc55998f1555c1b8dad
Ilya Volnistov <i.volnistov@gaijin.team>
2022-03-13 12:44:35 +0300

    add escaping for value formating

--- db/helpers.py
+++ db/helpers.py
@@ -1,12 +1,16 @@
 from typing import Any
 
 
+def escape_value(value: Any) -> str:
+    return str.replace(value, '\'', '\'\'')
+
+
 def value_fmt(*args: Any) -> str:
     def fmt(value):
-        return f'\'{str(value)}\''
+        return f'\'{escape_value(value)}\''
 
     return ', '.join(map(fmt, args))
 
 
 def where_fmt(**kwargs: str) -> str:
-    return ', '.join([f'{k} = \'{v}\'' for k, v in kwargs.items()])
+    return ', '.join([f'{k} = \'{escape_value(v)}\'' for k, v in kwargs.items()])
